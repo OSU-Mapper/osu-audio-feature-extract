87456e192a1403534f95f4c3cba9392c0cbbdda1
Willian Z <Willian@Willian-Zhang.com>
2017-04-16 15:41:45 -0400

    Modify note extract

--- aubio-demos/demo_notes.py
+++ aubio-demos/demo_notes.py
@@ -23,7 +23,22 @@ tolerance = 0.8
 
 notes_o = notes("default", win_s, hop_s, samplerate)
 
-print("%8s" % "time","[ start","vel","last ]")
+# cordinate
+cordinate_samples = [
+    (352, 332), (224, 232)
+]
+cordinate_samples_max = len(cordinate_samples) - 1
+cordinate_i = 0
+def cordinate_increase():
+    global cordinate_i
+    if cordinate_i == cordinate_samples_max:
+        cordinate_i = 0
+    else:
+        cordinate_i += 1
+    
+def cordinate_current():
+    global cordinate_i
+    return cordinate_samples[cordinate_i]
 
 # total number of frames read
 total_frames = 0
@@ -31,7 +46,17 @@ while True:
     samples, read = s()
     new_note = notes_o(samples)
     if (new_note[0] != 0):
-        note_str = ' '.join(["%.2f" % i for i in new_note])
-        print("%.6f" % (total_frames/float(samplerate)), new_note)
+        time  = total_frames/float(samplerate) * 1000
+        cordinate = cordinate_current()
+        #note_str = ' '.join(["%.2f" % i for i in new_note])
+        print("{x},{y},{time},{type},{hitSound},{addition}".format(
+            x=cordinate[0],
+            y=cordinate[1],
+            time=int(time),
+            type=1,
+            hitSound=0,
+            addition="0:0:0:0:"
+        ))
+        cordinate_increase()
     total_frames += read
     if read < hop_s: break
