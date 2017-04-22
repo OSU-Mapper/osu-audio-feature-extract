fdfc8c4597a42d3054241e2633b0ab40d097aaa5
Willian Z <Willian@Willian-Zhang.com>
2017-04-22 00:48:11 -0400

    Change to right ext on unzip.sh

--- unzip.sh
+++ unzip.sh
@@ -2,11 +2,10 @@
 
 mkdir -p "Data/Beatmaps" 
 
-ext=".osz.zip.osz"
+ext=".osz"
 for path in Data/Beatmap-Packs/*/**.osz ;do
     filename=$(basename -s "$ext" "$path")
     if unzip "$path" -d "Data/Beatmaps/$filename"; then
         rm $path
     fi
 done
-
