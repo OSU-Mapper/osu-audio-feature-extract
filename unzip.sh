125997a48745c937a6cf568e563d9df6cd4819c8
Willian Z <Willian@Willian-Zhang.com>
2017-04-22 00:48:41 -0400

    Fix remove path

--- unzip.sh
+++ unzip.sh
@@ -6,6 +6,6 @@ ext=".osz"
 for path in Data/Beatmap-Packs/*/**.osz ;do
     filename=$(basename -s "$ext" "$path")
     if unzip "$path" -d "Data/Beatmaps/$filename"; then
-        rm $path
+        rm "$path"
     fi
 done
